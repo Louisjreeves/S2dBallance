27a0c7e115605c1c07f3e28fa8e952c81c488868
Louisjreeves <79279019+Louisjreeves@users.noreply.github.com>
2023-07-25 01:25:26 -0500

    Update vmdown.ps1

--- vmdown.ps1
+++ vmdown.ps1
@@ -3,29 +3,21 @@
 #$cred = Get-Credential
 
  
-
- 
-
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+ Set-ExecutionPolicy Unrestricted -scope Process 
 
- 
 
 
 $mydownloads = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
 
 $MyTemp =(Get-Item $mydownloads).fullname
 
-$myuser=  $myuser=  $env:USERDOMAIN + "\" + $env:USERNAME
-
+ 
+ 
+$response = Invoke-WebRequest -Uri https://raw.githubusercontent.com/Louisjreeves/S2dBallance/main/3Prod_VMBAM7.24.ps1 -OutFile $mytemp\Prod_VMBAM.ps1
 
  
-
-   $response = Invoke-WebRequest -Uri https://raw.githubusercontent.com/Louisjreeves/S2dBallance/main/3Prod_VMBAM7.24.ps1 -OutFile $mytemp\Prod_VMBAM.ps1
-
-
- $activedirectory = "C:\Users\$myuser\Downloads\"
-
-set-location $activedirectory
+set-location $mydownloads
 
   .\Prod_VMBAM.ps1 
 
